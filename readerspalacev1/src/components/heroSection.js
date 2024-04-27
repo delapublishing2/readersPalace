6a0fd72e705a14e29d3566f433fd5cdb095811ab
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 17:04:19 -0400

    commit

---
+++ readerspalacev1/src/components/heroSection.js
@@ -0,0 +1,15 @@
+// src/components/HeroSection.js
+import React from 'react';
+import './HeroSection.css'; // Assuming you are using plain CSS
+
+const HeroSection = () => {
+  return (
+    <div className="herosection">
+      <h1>Welcome to Reader's Palace</h1>
+      <p>Explore the world of books and join our community!</p>
+      {/* Add more elements as needed */}
+    </div>
+  );
+};
+
+export default HeroSection;
--- readerspalacev1/src/pages/home.js
+++ readerspalacev1/src/pages/home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/home.css'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HeroSection from '../components/heroSection'
+// Import other sections
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Reader's Palace</h1>
-      {/* Other sections */}
+      <Navbar />
+      <HeroSection />
+      {/* Include other sections here */}
+      <Footer />
     </div>
   );
 };
