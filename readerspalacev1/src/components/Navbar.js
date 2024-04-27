2fc294b521b60b184b4b2a7928c1c91adda35427
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 19:56:40 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -1,25 +1,24 @@
 // src/components/Navbar.js
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/Navbar.css';
-import NavbarButton from './NavbarButton';
+
 const Navbar = ({ navigate }) => {
   return (
     <div className="app-wrapper">
-     <nav className="navigationbar">
-  <div className="container mx-auto flex justify-between items-center">
-    <ul className="flex space-x-6">
-      <li><a onClick={() => navigate('home')}>Reader's Palace</a></li>
-      <li><a onClick={() => navigate('home')}>Home</a></li>
-      <li><a onClick={() => navigate('events')}>Events</a></li>
-      <li><a onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
-      <li><a onClick={() => navigate('contests')}>Contests</a></li>
-      <li><a onClick={() => navigate('blogs')}>Blogs</a></li>
-      <li><a onClick={() => navigate('about-us')}>About Us</a></li>
-    </ul>
-    {/* Add SearchBarComponent here */}
-  </div>
-  <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton>
-</nav>
+      <nav className="navigationbar">
+        <div className="container mx-auto flex justify-between items-center">
+          <ul className="flex space-x-6">
+            <li><a onClick={() => navigate('home')}>Reader's Palace</a></li>
+            <li><a onClick={() => navigate('home')}>Home</a></li>
+            <li><a onClick={() => navigate('events')}>Events</a></li>
+            <li><a onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
+            <li><a onClick={() => navigate('contests')}>Contests</a></li>
+            <li><a onClick={() => navigate('blogs')}>Blogs</a></li>
+            <li><a onClick={() => navigate('about-us')}>About Us</a></li>
+          </ul>
+        </div>
+        <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton>
+      </nav>
     </div>
   );
 };
