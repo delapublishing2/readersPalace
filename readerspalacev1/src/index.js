5abaf40bcad42557f8f123cda644eddda7c125bd
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 13:16:41 -0400

    commit

---
+++ readerspalacev1/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/App.js'; // Ensure App.js is correctly set up to export a React component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
