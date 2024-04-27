ce5c5109f93ed3f28c6acb3256b10a915b06f292
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 18:57:51 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -1,30 +1,15 @@
 // src/components/Navbar.js
 import React from 'react';
+import Button from './JoinButton';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ navigate }) => {
   return (
-    <div className="app-wrapper">
-      <nav className="navigationBar">
-        <div className="container mx-auto flex justify-between items-center">
-          <Link to="/" className="brand">
-            <span>Reader's Palace</span>
-          </Link>
-          <ul className="nav-links">
-            <li><Link to="/about-us">About Us</Link></li>
-            <li><Link to="/events">Events</Link></li>
-            <li><Link to="/blogs">Blogs</Link></li>
-            <li><Link to="/book-clubs">Book Clubs</Link></li>
-            <li><Link to="/contests">Contests</Link></li>
-            <li><Link to="/resources">Resources</Link></li>
-            <li><Link to="/authors-spotlight">Author's Spotlight</Link></li>
-            <li><Link to="/forums">Forums</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-          </ul>
-          {/* Include SearchBarComponent if needed */}
-        </div>
-      </nav>
-    </div>
+    <nav>
+      <Button onClick={() => navigate('home')}>Home</Button>
+      <Button onClick={() => navigate('about-us')}>About Us</Button>
+      {/* Add other navigation buttons */}
+    </nav>
   );
 };
 
