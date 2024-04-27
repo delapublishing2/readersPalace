01cf351fe79425c1209eef11f1b5d10b4eccd25f
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 18:21:03 -0400

    commit

--- readerspalacev1/src/components/heroSection.js
+++ readerspalacev1/src/components/heroSection.js
@@ -1,19 +1,23 @@
 // src/components/HeroSection.js
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/heroSection.css'; 
+import Button from './Button'; // Assuming Button is in the same directory
 
 const HeroSection = () => {
   return (
     <section id="welcome" className="herosection">
       <div className="hero-section-header">
-        <h1>Welcome to Reader's Palace</h1>
+        <h1>Welcome to Reader's</h1>
+        <h1> Palace</h1>
       </div>
       <div className="hero-section-text">
-        <p>Unlock the Majesty of Storytelling at Reader's Palace - A regal haven for book lovers.</p>
-        <p>Explore a world where every story matters, and become part of a community that cherishes the art of storytelling.</p>
+      <p>Unlock the Majesty of Storytelling at Reader's Palace - A regal </p>
+      <p>haven for book lovers.</p>
+        <p>Explore a world where every story matters, </p>
+        <p>and become part of a community that cherishes the art of storytelling.</p>
       </div>
       <a href="/signup">
-        <button className="join-button">Join the Community</button>
+        <Button className="join-button">Join the Community</Button>
       </a>
     </section>
   );
