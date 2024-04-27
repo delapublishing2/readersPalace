716a56a01a2d4239137d19e7bec14a90fc86a6a3
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 18:35:33 -0400

    commit

--- readerspalacev1/src/components/Button.js
+++ readerspalacev1/src/components/Button.js
@@ -2,11 +2,10 @@
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/Button.css';
 
-const Button = ({ onClick, children, className }) => {
-  // Combine provided className with default button classes
-  const buttonClass = `button ${className}`;
+const Button = ({ onClick, children, className = '', type = 'button' }) => {
+  const buttonClass = `join-button ${className}`; // Use join-button class for base styling
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={buttonClass} onClick={onClick} type={type}>
       {children}
     </button>
   );
