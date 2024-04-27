beda0428596c325f6a638861bba3da480799423b
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 19:50:55 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -5,21 +5,21 @@ import NavbarButton from './NavbarButton';
 const Navbar = ({ navigate }) => {
   return (
     <div className="app-wrapper">
-      <nav className="navigationbar">
-        <div className="container mx-auto flex justify-between items-center">
-          <a onClick={() => navigate('home')}>Reader's Palace</a>
-          <ul className="flex space-x-6">
-            <li><a onClick={() => navigate('home')}>Home</a></li>
-            <li><a onClick={() => navigate('events')}>Events</a></li>
-            <li><a onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
-            <li><a onClick={() => navigate('contests')}>Contests</a></li>
-            <li><a onClick={() => navigate('blogs')}>Blogs</a></li>
-            <li><a onClick={() => navigate('about-us')}>About Us</a></li>
-          </ul>
-          {/* Add SearchBarComponent here */}
-        </div>
-        <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton>
-      </nav>
+     <nav className="navigationbar">
+  <div className="container mx-auto flex justify-between items-center">
+    <ul className="flex space-x-6">
+      <li><a onClick={() => navigate('home')}>Reader's Palace</a></li>
+      <li><a onClick={() => navigate('home')}>Home</a></li>
+      <li><a onClick={() => navigate('events')}>Events</a></li>
+      <li><a onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
+      <li><a onClick={() => navigate('contests')}>Contests</a></li>
+      <li><a onClick={() => navigate('blogs')}>Blogs</a></li>
+      <li><a onClick={() => navigate('about-us')}>About Us</a></li>
+    </ul>
+    {/* Add SearchBarComponent here */}
+  </div>
+  <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton>
+</nav>
     </div>
   );
 };
