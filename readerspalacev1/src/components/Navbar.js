994cf3720f3e7040a29e555188170fc206e6b8b9
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 19:13:52 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -5,11 +5,22 @@ import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles
 
 const Navbar = ({ navigate }) => {
   return (
-    <nav>
-      <Button onClick={() => navigate('home')}>Home</Button>
-      <Button onClick={() => navigate('about-us')}>About Us</Button>
-      {/* Add other navigation buttons */}
-    </nav>
+    <div className="app-wrapper">
+      <nav className="navigationbar">
+        <div className="container mx-auto flex justify-between items-center">
+          <Button onClick={() => navigate('home')}>Reader's Palace</Button>
+          <ul className="flex space-x-6">
+            <li><Button onClick={() => navigate('home')}>Home</Button></li>
+            <li><Button onClick={() => navigate('events')}>Events</Button></li>
+            <li><Button onClick={() => navigate('bookclubs')}>Book Clubs</Button></li>
+            <li><Button onClick={() => navigate('contests')}>Contests</Button></li>
+            <li><Button onClick={() => navigate('blogs')}>Blogs</Button></li>
+            <li><Button onClick={() => navigate('about-us')}>About Us</Button></li>
+          </ul>
+          {/* Add SearchBarComponent here */}
+        </div>
+      </nav>
+    </div>
   );
 };
 
