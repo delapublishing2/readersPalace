420b43c6ecccc535270b1e503288e40841971ea2
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 17:38:18 -0400

    commit

--- readerspalacev1/src/components/heroSection.js
+++ readerspalacev1/src/components/heroSection.js
@@ -1,17 +1,15 @@
 // src/components/HeroSection.js
 import React from 'react';
-import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/heroSection.css'; // Assuming you are using plain CSS
+import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/heroSection.css'; // Ensure this CSS file includes the updated styles
 
 const HeroSection = () => {
   return (
     <div className="herosection">
-        <div className="hero-section-container" >
         <div className="hero-section-header">
         <h1>Welcome to Reader's Palace</h1>
         </div>
      <div className="hero-section-text">
      <p>Explore the world of books and join our community!</p>
-     </div>
      </div>
      
       {/* Add more elements as needed */}
