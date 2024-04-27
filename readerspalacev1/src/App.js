1c2b6545c8f33afb8f09c103a96d5df56c7df61d
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 15:49:26 -0400

    commit

--- readerspalacev1/src/App.js
+++ readerspalacev1/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home';
+import AboutUs from './pages/AboutUs';
+import Events from './pages/Events';
+// Import other pages
+
+import Navbar from './components/Navbar'; // Assuming Navbar is a component
+import Footer from './components/Footer'; // Assuming Footer is a component
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about-us" component={AboutUs} />
+        <Route path="/events" component={Events} />
+        {/* Define other routes */}
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
-}
+const Navbar = () => {
+    return (
+        <nav style={{ background: 'var(--deep-blue)', color: 'var(--ivory)' }}>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about-us">About Us</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/blogs">Blogs</Link></li>
+                <li><Link to="/book-clubs">Book Clubs</Link></li>
+                <li><Link to="/contests">Contests</Link></li>
+                <li><Link to="/resources">Resources</Link></li>
+                <li><Link to="/authors-spotlight">Author's Spotlight</Link></li>
+                <li><Link to="/forums">Forums</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+            </ul>
+        </nav>
+    );
+};
 
 export default Navbar;
