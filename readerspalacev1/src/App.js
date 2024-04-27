345268ba257e316d6745c5de01787cd149148a94
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 17:09:51 -0400

    commit

--- readerspalacev1/src/App.js
+++ readerspalacev1/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/App.css';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
