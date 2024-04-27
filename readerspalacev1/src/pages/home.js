973ed0dd70e7e77d8264b870fdf6960636705d69
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 17:07:07 -0400

    commit

--- readerspalacev1/src/pages/home.js
+++ readerspalacev1/src/pages/home.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/home.css'
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import HeroSection from '../components/heroSection'
 // Import other sections
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
+ 
       <HeroSection />
       {/* Include other sections here */}
-      <Footer />
+     
     </div>
   );
 };
