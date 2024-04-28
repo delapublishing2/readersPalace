40b953b859694ad1257ecbb1fcf425ccfac5fc59
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 11:54:25 -0400

    commit

--- readerspalacev1/src/App.js
+++ readerspalacev1/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState } from 'react';
 import Home from './pages/home';
-import AboutUs from './pages/AboutUs';
+import AboutUs from '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/pages/AboutUs.js';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/App.css';
@@ -18,7 +18,7 @@ function App() {
       <div>
   <Navbar navigate={navigate} />
   {currentPage === 'home' && <Home />} // This will render the Home component if currentPage is 'home'
-  {currentPage === 'AboutUs' && <AboutUs />} // This will render the AboutUs component if currentPage is 'AboutUs'
+{currentPage === 'AboutUs' && <AboutUs />} // This will render the AboutUs component if currentPage is 'AboutUs'
   {/* Add other conditional renders for different pages */}
       <Footer />
     </div>
