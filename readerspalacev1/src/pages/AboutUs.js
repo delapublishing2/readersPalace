3403c02d2fc1edeb762744a5835d268b34bd68d6
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 11:51:57 -0400

    commit

--- readerspalacev1/src/pages/AboutUs.js
+++ readerspalacev1/src/pages/AboutUs.js
@@ -4,17 +4,19 @@ import HeaderSection from '../components/HeaderSection.js';
 import HeroSection from '../components/heroSection.js';
 
 const AboutUs = () => {
-  return (
-    <div>
-      <HeaderSection 
-        title="About Us" 
-        subtitle="Introducing Reader's Palace and its dedication to celebrating the love of literature."
-        imageSrc="/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/assets/ReadersPalaceContact.JPG" 
-      />
-      <p> hi</p>
-    </div>
-  );
-};
+    return (
+      <div>
+        <HeaderSection 
+          title="About Us" 
+          subtitle="Introducing Reader's Palace and its dedication to celebrating the love of literature."
+          imageSrc="/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/assets/ReadersPalaceContact.JPG" 
+        />
+        <p> hi</p>
+        <HeroSection />
+      </div>
+    );
+  };
+
 <HeroSection />
 
 export default AboutUs
