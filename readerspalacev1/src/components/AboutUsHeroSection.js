72bf45afde15aa9d0e6af50d79dc114978471de5
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 23:29:52 -0400

    commit

---
+++ readerspalacev1/src/components/AboutUsHeroSection.js
@@ -0,0 +1,22 @@
+// src/components/AboutUsHeroSection.js
+import React from 'react';
+import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/heroSection.css'; 
+import Button from './JoinButton'; // Assuming Button is a custom component styled according to your project's needs
+
+const AboutUsHeroSection = () => {
+  return (
+    <section className="herosection">
+      <div className="background-image"></div> {/* Background image is handled via CSS */}
+      <div className="overlay"></div> {/* Overlay is handled via CSS */}
+      <div className="content">
+        <h1>About Us</h1>
+        <p>Learn more about Reader's Palace and our mission to promote the love of reading.</p>
+        <a href="/signup" className="join-button">
+          <Button>Join the Community</Button>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUsHeroSection;
