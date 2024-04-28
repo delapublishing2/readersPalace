5ef5eb4f3d6e43ddcafd2192033ffc65492aeef6
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 11:25:14 -0400

    commit

---
+++ readerspalacev1/src/pages/AboutUs.js
@@ -0,0 +1,18 @@
+// src/pages/AboutUs.js
+import React from 'react';
+import HeaderSection from '../components/HeaderSection';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <HeaderSection 
+        title="About Us" 
+        subtitle="Introducing Reader's Palace and its dedication to celebrating the love of literature."
+        imageSrc="/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/assets/ReadersPalaceContact.JPG" 
+      />
+      {/* Rest of the About Us page content */}
+    </div>
+  );
+};
+
+export default AboutUs;
