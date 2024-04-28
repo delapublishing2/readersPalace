1d5efff4eb28b85ca932b35ffe6a9a567b472b3c
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 20:08:49 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -8,16 +8,17 @@ const Navbar = ({ navigate }) => {
       <nav className="navigationbar">
         <div className="container mx-auto flex justify-between items-center">
           <ul className="flex space-x-6">
-            <li><a onClick={() => navigate('home')}>Reader's Palace</a></li>
+          <li><a className="readers-palace" onClick={() => navigate('home')}>Reader's Palace</a></li>
             <li><a onClick={() => navigate('home')}>Home</a></li>
             <li><a onClick={() => navigate('events')}>Events</a></li>
             <li><a onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
             <li><a onClick={() => navigate('contests')}>Contests</a></li>
             <li><a onClick={() => navigate('blogs')}>Blogs</a></li>
             <li><a onClick={() => navigate('about-us')}>About Us</a></li>
+            <li> <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton></li>
           </ul>
         </div>
-        <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton>
+        
       </nav>
     </div>
   );
