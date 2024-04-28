410a329b9a81ec3f2dad13df8f78adbc093440da
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 13:26:33 -0400

    commit

--- readerspalacev1/src/pages/AboutUs.js
+++ readerspalacev1/src/pages/AboutUs.js
@@ -1,22 +1,18 @@
-// src/pages/AboutUs.js
-import React from 'react';
-import HeaderSection from '../components/HeaderSection.js';
-import HeroSection from '../components/heroSection.js';
-import ReadersPalaceAboutUs from '.readerspalacev1/src/assets/ReadersPalaceAboutUs.JPG';
+// src/pages/AboutUs.js 
+import React from 'react'; 
+import HeaderSection from '../components/HeaderSection.js'; 
+import ReadersPalaceAboutUs from '.readerspalacev1/src/assets/ReadersPalaceAboutUs.JPG'; 
 
-
-const AboutUs = () => {
-    return (
-      <div>
-<HeaderSection 
-  title="About Us" 
-  subtitle="Introducing Reader's Palace and its dedication to celebrating the love of literature."
-  imageSrc={ReadersPalaceContact} 
-/>
-        <p> hi</p>
-        <HeroSection />
-      </div>
-    );
-};
+const AboutUs = () => { 
+  return ( 
+    <div> 
+      <HeaderSection 
+        title="About Us" 
+        subtitle="Introducing Reader's Palace and its dedication to celebrating the love of literature." 
+        imageSrc={ReadersPalaceAboutUs}
+      /> 
+    </div> 
+  ); 
+}; 
 
 export default AboutUs;
