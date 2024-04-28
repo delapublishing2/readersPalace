0e91044a98a6884c2964f1113d9e9769cafd033c
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 20:45:03 -0400

    commit

--- readerspalacev1/src/components/Navbar.js
+++ readerspalacev1/src/components/Navbar.js
@@ -5,26 +5,23 @@ import NavbarButton from './NavbarButton.js';
 const Navbar = ({ navigate }) => {
   return (
     <div className="app-wrapper">
-    <nav className="navigationbar">
-  <div className="container mx-auto flex justify-between items-center">
-    <div>
-      <li><a className="readers-palace" onClick={() => navigate('home')}>Reader's Palace</a></li>
-    </div>
-    <div>
-      <ul className="flex space-x-6">
-        <li><a className="navbar-link" onClick={() => navigate('home')}>Home</a></li>
-        <li><a className="navbar-link" onClick={() => navigate('events')}>Events</a></li>
-        <li><a className="navbar-link" onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
-        <li><a className="navbar-link" onClick={() => navigate('contests')}>Contests</a></li>
-        <li><a className="navbar-link" onClick={() => navigate('blogs')}>Blogs</a></li>
-        <li><a className="navbar-link" onClick={() => navigate('about-us')}>About Us</a></li>
-      </ul>
-    </div>
-    <div>
-      <li> <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton></li>
-    </div>
+      <nav className="navigationbar">
+        <div className="container mx-auto flex justify-between items-center">
+        <ul className="flex space-x-6">
+  <div>
+    <li><a className="readers-palace" onClick={() => navigate('home')}>Reader's Palace</a></li>
+    <li><a className="navbar-link" onClick={() => navigate('home')}>Home</a></li>
+<li><a className="navbar-link" onClick={() => navigate('events')}>Events</a></li>
+<li><a className="navbar-link" onClick={() => navigate('bookclubs')}>Book Clubs</a></li>
+<li><a className="navbar-link" onClick={() => navigate('contests')}>Contests</a></li>
+<li><a className="navbar-link" onClick={() => navigate('blogs')}>Blogs</a></li>
+<li><a className="navbar-link" onClick={() => navigate('about-us')}>About Us</a></li>
   </div>
-</nav>
+  <li> <NavbarButton onClick={() => navigate('signup')}>Sign Up</NavbarButton></li>
+</ul>
+        </div>
+        
+      </nav>
     </div>
   );
 };
