27b49d2564f762e43e97ab83b2b497b8335550b8
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 11:27:39 -0400

    commit

--- readerspalacev1/src/components/HeaderSection.js
+++ readerspalacev1/src/components/HeaderSection.js
@@ -1,17 +1,17 @@
 // src/components/Header.js
 import React from 'react';
-import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/Header.css'; 
+import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/HeaderSection.css'; 
 import Button from './buttons/JoinButton.js'; 
 
-const HeaderSection = ({ imageSrc }) => {
+const HeaderSection = ({ title, subtitle, imageSrc }) => {
     return (
       <div className="header-section">
         <img src={imageSrc} alt="Header Image" />
         <h1>{title}</h1>
-      <p>{subtitle}</p>
-      <Button />
-    </div>
-  );
+        <p>{subtitle}</p>
+        <Button />
+      </div>
+    );
 };
 
 export default HeaderSection;
