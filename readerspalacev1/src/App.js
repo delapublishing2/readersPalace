3b29360b41d2063e0285f0041d675cef9531647b
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-27 22:24:55 -0400

    commit

--- readerspalacev1/src/App.js
+++ readerspalacev1/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/App.css';
 
+
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
 
--- readerspalacev1/src/pages/home.js
+++ readerspalacev1/src/pages/home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '/Users/jazzyme/Documents/GitHub/readersPalace/readerspalacev1/src/styles/home.css'
 import HeroSection from '../components/heroSection'
+import FeatureSection from '../components/FeatureSection';
 // Import other sections
 
 const Home = () => {
   return (
     <div>
- 
       <HeroSection />
+      <FeatureSection />
       {/* Include other sections here */}
-     
     </div>
   );
 };
