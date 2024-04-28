dcf6d511e0e6f742d7a7be01a7ebd16192199e4e
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-04-28 12:17:03 -0400

    commit

--- readerspalacev1/src/App.js
+++ readerspalacev1/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer.js'; // Assuming you have a Footer component
 import HomePage from './pages/home.js';
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about-us" component={AboutUsPage} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
         {/* Add more routes as needed */}
-      </Switch>
+      </Routes>
       <Footer />
     </Router>
   );
